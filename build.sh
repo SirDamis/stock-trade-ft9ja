77b728bc3ad8c695e2168fd6074721226706cef0
sirdamis <adedejidamisola@gmail.com>
2023-06-25 03:57:17 +0100

    Added the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
 set -o errexit  # exit on error
+
+# Start the Celery worker
+celery -A core worker --loglevel=info &
+
+# Start the Celery beat scheduler
+celery -A core beat --loglevel=info &
 
 pip install -r requirements.txt
 
+# Start the Django server
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_traders
+
+wait
