e05114bd06d8fdb4ac68b8917c874e7021b3f98f
sirdamis <adedejidamisola@gmail.com>
2023-06-25 04:55:10 +0100

    Added the static details

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import django_eventstream
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("trade.urls")),
     path("", include("user.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
