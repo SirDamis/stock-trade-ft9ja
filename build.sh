beddfba2c328f7d44e8ff4d9d2e7cd9dc36d433b
sirdamis <adedejidamisola@gmail.com>
2023-06-25 04:46:20 +0100

    updated the build file

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -o errexit  # exit on error
 
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
 # Start the Celery worker
 celery -A core worker --loglevel=info &
 
 # Start the Celery beat scheduler
 celery -A core beat --loglevel=info &
 
-pip install -r requirements.txt
-
 # Start the Django server
-python -m pip install --upgrade pip
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_traders
